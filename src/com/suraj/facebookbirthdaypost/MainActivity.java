a02081f4ba84f8ef1e5992d141b996f11784c035
Suraj Dubey <sdubey.dubey14@gmail.com>
2014-07-21 21:06:13 +0530

    Display name after successful login

--- src/com/suraj/facebookbirthdaypost/MainActivity.java
+++ src/com/suraj/facebookbirthdaypost/MainActivity.java
@@ -1,13 +1,7 @@
 package com.suraj.facebookbirthdaypost;
 
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.Request.GraphUserCallback;
-import com.facebook.model.GraphUser;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,51 +9,28 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	Button login;
+	final Context context = this;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		login = (Button) findViewById(R.id.login);
-		
-		Session.openActiveSession(this, true, new Session.StatusCallback() {
+		login.setOnClickListener(new OnClickListener() {
 			
 			@Override
-			public void call(Session session, SessionState state, Exception exception) {
-				// TODO Auto-generated method stub
-				
-				if(session.isOpened())
-				{
-					// make request to the /me API
-					Request.newMeRequest(session, new GraphUserCallback() {
-						
-						@Override
-						public void onCompleted(GraphUser user, Response response) {
-							if(user!=null)
-							{
-								login.setText("Hello " + user.getName()+"!");
-								
-							}
-						}
-					}).executeAsync();
-					
-				}
-				
+			public void onClick(View v) {
+				Intent intent = new Intent(context , FacebookLoginActivity.class);
+				startActivity(intent);
 			}
 		});
+		
 	}
 	
-	 @Override
-	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	      super.onActivityResult(requestCode, resultCode, data);
-	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-	  }
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
